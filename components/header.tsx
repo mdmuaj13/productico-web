d9e3e52c590fbb0b3c3c4b9753507faeca695c26
Mohaiminul Hasan <iammuaj@gmail.com>
2026-03-25 11:25:13 +0600

    fix horizontal scroll

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ const menuItems = [
 	{ name: 'Features', href: '#features' },
 	{ name: 'Solution', href: '#solution' },
 	{ name: 'Pricing', href: '#pricing' },
-	{ name: 'Faq', href: '#faqs' },
+	{ name: 'FAQ', href: '#faqs' },
 ];
 
 export const Header = () => {
@@ -69,7 +69,7 @@ export const Header = () => {
 							<button
 								onClick={() => setMenuState(!menuState)}
 								aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
-								className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden">
+								className="relative z-20 -m-2.5 block cursor-pointer p-2.5 lg:hidden">
 								<Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
 								<X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
 							</button>
